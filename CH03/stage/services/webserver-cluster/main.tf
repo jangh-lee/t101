3842cda0329b226349e25af0cfe006c974f03d0a
jangh-lee <72970232+jangh-lee@users.noreply.github.com>
2022-11-06 03:01:00 +0900

    Update main.tf

--- CH03/stage/services/webserver-cluster/main.tf
+++ CH03/stage/services/webserver-cluster/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "$NICKNAME-t101study-tfstate-week3-files"
+    bucket = "janghun-t101study-tfstate-week3-files"
     key    = "stage/services/webserver-cluster/terraform.tfstate"
     region = "ap-northeast-2"
     dynamodb_table = "terraform-locks-week3-files"
@@ -14,7 +14,7 @@ provider "aws" {
 data "terraform_remote_state" "db" {
   backend = "s3"
   config = {
-    bucket = "$NICKNAME-t101study-tfstate-week3-files"
+    bucket = "janghun-t101study-tfstate-week3-files"
     key    = "stage/data-stores/mysql/terraform.tfstate"
     region = "ap-northeast-2"
   }
