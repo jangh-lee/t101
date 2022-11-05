673682f03857ab5f7140e9b8435bae655b6a7ccc
jangh-lee <72970232+jangh-lee@users.noreply.github.com>
2022-11-06 01:46:19 +0900

    Create backend.tf

---
+++ CH03/workspace/backend/backend.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_s3_bucket" "mys3bucket" {
+  bucket = "janghun-t101study-tfstate-week3"
+}
+
+resource "aws_s3_bucket_versioning" "mys3bucket_versioning" {
+  bucket = aws_s3_bucket.mys3bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "mydynamodbtable" {
+  name         = "terraform-locks-week3"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.mys3bucket.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.mydynamodbtable.name
+  description = "The name of the DynamoDB table"
+}
