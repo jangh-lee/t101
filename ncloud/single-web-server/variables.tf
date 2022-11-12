3791d6b8e63ac05e4e6d2ff82409e384d48033f6
jangh-lee <72970232+jangh-lee@users.noreply.github.com>
2022-11-13 08:51:15 +0900

    Create variables.tf

---
+++ ncloud/single-web-server/variables.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+  default = "엑세스 키"
+}
+
+variable "secret_key" {
+  default = "시크릿 키"
+}
+
+variable "region" {
+  default = "KR"
+}
+
+variable "site" {
+  default = "public"
+}
+
+variable "support_vpc" {
+  default = "true"
+}
