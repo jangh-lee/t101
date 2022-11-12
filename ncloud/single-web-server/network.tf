0b828fea80e22050e5f6948f49ed03c89ac85025
jangh-lee <72970232+jangh-lee@users.noreply.github.com>
2022-11-13 08:51:45 +0900

    Create network.tf

---
+++ ncloud/single-web-server/network.tf
@@ -0,0 +1,13 @@
+resource "ncloud_vpc" "vpc" {
+  name = "web-dev-vpc"
+  ipv4_cidr_block = "10.0.0.0/16"
+}
+
+resource "ncloud_subnet" "subnet" {
+  vpc_no         = ncloud_vpc.vpc.id
+  subnet         = "10.0.1.0/24"
+  zone           = "KR-2"
+  network_acl_no = ncloud_vpc.vpc.default_network_acl_no
+  subnet_type    = "PUBLIC"
+  name           = "subnet-01"
+}
