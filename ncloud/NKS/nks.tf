885b9414a086fbf5b21d0fb65463b5be94c574b2
jangh-lee <72970232+jangh-lee@users.noreply.github.com>
2022-11-14 14:27:50 +0900

    Update nks.tf

--- ncloud/NKS/nks.tf
+++ ncloud/NKS/nks.tf
@@ -27,3 +27,17 @@ resource "ncloud_nks_cluster" "cluster" {
     audit = true
   }
 }
+
+
+resource "ncloud_nks_node_pool" "node_pool" {
+  cluster_uuid   = ncloud_nks_cluster.cluster.uuid
+  node_pool_name = "sample-node-pool"
+  node_count     = 2
+  product_code   = "SVR.VSVR.STAND.C002.M008.NET.SSD.B050.G002"
+  subnet_no      = ncloud_subnet.k8s-cluster-kr1.id
+  autoscale {
+    enabled = true
+    min = 1
+    max = 2
+  }
+}
