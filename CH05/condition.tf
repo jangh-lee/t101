e36b356d0c08671aaa7c1aa7e8ba1b09e0736d1a
jangh-lee <72970232+jangh-lee@users.noreply.github.com>
2022-11-20 02:30:10 +0900

    Update condition.tf

--- CH05/condition.tf
+++ CH05/condition.tf
@@ -1,12 +1,22 @@
-variable "fruits" {
-  description = "fruits list"
-  type = list(string)
-  default = ["apple", "banana", "strawberry"]
+# link : https://registry.terraform.io/providers/NaverCloudPlatform/ncloud/latest/docs/resources/block_storage
+# bool 타입으로 envtype을 받게되는데, true값이면 3, false값이면 1개의 block storage를 생성한다.
+
+
+
+resource "ncloud_block_storage" "storage" {
+    server_instance_no = ncloud_server.server.id
+    name = "storage-${count.index}"
+    count = var.envtype ? 3 : 1
+    size = "10"
 }
 
-output "for_directive_index_if" {
-  value = <<EOF
-%{ for i, fruits in var.fruits }
-  ${fruits}%{ if i < length(var.fruits) - 1 }, %{ endif }
-%{ endfor }
-EOF
+variable "envtype" {
+  type = bool
+}
+
+resource "ncloud_server" "server" {
+  subnet_no                 = ncloud_subnet.test.id
+  name                      = "my-tf-server"
+  server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"
+  login_key_name            = ncloud_login_key.loginkey.key_name
+}
